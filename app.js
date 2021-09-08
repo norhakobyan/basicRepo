dd5bec384225d4e91461a9b5d0c67f8172276dce
Norayr <norayr.hakobyan@all.me>
2021-09-08 21:17:51 +0400

    add login logOut

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const User = require('./users/index');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
-const userDb = new User();
+app.use(express.urlencoded({ extended: false }));
+const user = new User();
 
 app.post('/reg', async (req, res) => {
   try {
@@ -15,13 +15,46 @@ app.post('/reg', async (req, res) => {
       return res.send('Invalid registration data');
     }
 
-    const result = await userDb.create(req.body);
+    const result = await user.create(req.body);
 
     return res.send(result);
   } catch(e) {
     return res.send('Something went wrong');
   }
-})
+});
+
+app.post('/login', async (req, res) => {
+
+  try {
+    const { email, password } = req.body;
+    if(!email || !password) {
+    return res.send('Invalid login data');
+  }
+
+  const result = await user.login(email, password);
+  return res.send(result);
+
+  } catch(e) {
+    return res.send('Something went wrong');
+  }
+});
+
+app.post('/logOut', async (req, res) => {
+
+  try {
+    const { token } = req.body;
+    if(!token) {
+    return res.send('Invalid logOut data or token');
+    }
+
+  const result = await user.logOut(token);
+  return res.send(result);
+
+  } catch(e) {
+      return res.send('Something went wrong');
+  }
+});
+
 
 app.listen(3000);
 
--- users/index.js
+++ users/index.js
@@ -1,4 +1,11 @@
+const redis = require("redis");
+var jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+
 const { UserModel } = require('../db/index');
+
+const client = redis.createClient();
 
 class User {
   constructor() {};
@@ -11,6 +18,29 @@ class User {
     }
     const result = await UserModel.create(user);
     return result;
+  }
+
+  async login(email, password) {
+
+    const user = await UserModel.findOne({ email, password });
+    if(!user) {
+      throw new Error('User not found!');
+    }
+    const token = jwt.sign({ userId: user.id }, 'basicItCenter');
+    console.log(token);
+    await promisify(client.set).bind(client)(user.id, token);
+    return { token };
+  }
+
+  async logOut(token) {
+
+    const { userId } = jwt.verify(token, 'basicItCenter');
+    const user = await UserModel.findOne({ id: userId });
+    if(!user) {
+      throw new Error('Sonething went wrong');
+    }
+    await promisify(client.del).bind(client)(user.id);
+    return 'success'
   }
 }
 
